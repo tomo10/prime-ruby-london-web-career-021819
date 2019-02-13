aa14540b7c54b5940026e8115905c61efe0e85db
Thomas Edwards <thce28@gmail.com>
2019-02-13 12:58:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+require 'pry'
+
+def prime?(n)
+  foundDivider = false
+    if n <= 1 
+    foundDivider = true
+    else
+      for d in 2..(n - 1)
+      foundDivider = ((n % d) == 0) || foundDivider
+      binding.pry 
+      end 
+   end
+  not foundDivider
+ end
+ 
+ 
